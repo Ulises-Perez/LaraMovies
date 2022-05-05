0505172bb76f6eaefe61e256b8ba1093c4a3f822
Ulises-Perez <ulisesalbertoperez15@gmail.com>
2022-05-04 23:51:57 -0400

    cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -114,6 +114,31 @@
             </div>
         </section>
 
+        <section>
+            <div class="w-full py-20">
+                <div class="container mx-auto px-4 xl:px-0 text-white mb-10">
+                    <p class="text-2xl md:text-3xl tracking-wide" id="movie-name">
+                        Peliculas Tendencias
+                    </p>
+                    <p class="text-sm text-red-400">Solo en Streaming</p>
+                </div>
+                <div class="container mx-auto flex justify-center content-center px-4 xl:px-0 mb-6">
+                    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 justify-center content-center text-white">
+                        @php
+                            $i=0;
+                        @endphp
+                        @foreach ($trendingMovies['results'] as $movies)
+                            @if ($i++ <=11)
+
+                                <x-movie-card :movies="$movies"/>
+                                
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </section>
+
         <!--<section class="w-full my-20">
             <div class="container mx-auto px-4 xl:px-0 text-white">
                 <div class="box-list-box rounded-xl shadow-lg shadow-gray-100/25">
